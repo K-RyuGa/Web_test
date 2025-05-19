46db40c360cb6392f065d6d8d5cb2345c6ae6626
K-RyuGa <y24m002@mail.ryukoku.ac.jp>
2025-05-19 13:41:41 +0900

    ログイン＋シークレット

--- secret_test.py
+++ secret_test.py
@@ -4,6 +4,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import json
 
+
 # --- Google Sheets 認証 ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds_dict = st.secrets["gcp_service_account"]
