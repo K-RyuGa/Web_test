9968e11fc3245bb983d8624033bacf2ab2af3674
K-RyuGa <y24m002@mail.ryukoku.ac.jp>
2025-06-19 11:18:06 +0900

    履歴管理機能

--- secret_test.py
+++ secret_test.py
@@ -140,7 +140,7 @@ if st.session_state.logged_in:
                 st.session_state["chat_history"] = []
                 st.session_state["clear_screen"] = False
                 st.session_state["chat"] = False
-                del st.session_state["style_label"]
+                del style_label
                 st.rerun()
                 
         else:
